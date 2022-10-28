7873acad58c4d1f960c6ed8afdbe6759778b3cd8
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-28 10:30:03 +0300

    Add files via upload

---
+++ doxigen/html/_look_up_boss___n_s_8cpp.js
@@ -0,0 +1,15 @@
+var _look_up_boss___n_s_8cpp =
+[
+    [ "_CRT_SECURE_NO_WARNINGS", "_look_up_boss___n_s_8cpp.html#af08ec37a8c99d747fb60fa15bc28678b", null ],
+    [ "exists", "_look_up_boss___n_s_8cpp.html#a4dee73c7e2036bd114f1c272452a3f34", null ],
+    [ "GettingIdFromTxtFile", "_look_up_boss___n_s_8cpp.html#a54670943228b38c6b5e433181c65c4fd", null ],
+    [ "isdigit", "_look_up_boss___n_s_8cpp.html#a8aa99ff0da793e57b85a5107c349c868", null ],
+    [ "main", "_look_up_boss___n_s_8cpp.html#a0ddf1224851353fc92bfbff6f499fa97", null ],
+    [ "MatchingIdAndName", "_look_up_boss___n_s_8cpp.html#a68188efb463150ad7db69c69da3a174a", null ],
+    [ "SavingResultsToAFile", "_look_up_boss___n_s_8cpp.html#aef853e68ad2c42d6b5113a195b6234ee", null ],
+    [ "SearchSuperiorsOfTheDesiredEmployee", "_look_up_boss___n_s_8cpp.html#ae03be944b432305f4fe4c0d2048e5dc4", null ],
+    [ "WorkingWithXMLFile", "_look_up_boss___n_s_8cpp.html#ac543fca4fdec6ba37b8bfe58922b1521", null ],
+    [ "habsent", "_look_up_boss___n_s_8cpp.html#acd71ead6883f4171bffda8f887911dcf", null ],
+    [ "headers", "_look_up_boss___n_s_8cpp.html#a0fd5e924887b930e112b486a5191d47d", null ],
+    [ "person", "_look_up_boss___n_s_8cpp.html#a0d44b0656c7f127f85a3fcc94d6d075f", null ]
+];
---
+++ doxigen/html/struct_person.js
@@ -0,0 +1,5 @@
+var struct_person =
+[
+    [ "id", "struct_person.html#aec48a92f614a854ff380a15eb8e2f479", null ],
+    [ "Name", "struct_person.html#ae5f6f08da8040518bc5993fafb340de5", null ]
+];
